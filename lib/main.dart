af85f18c9c3e61ef47c65eb83de7b21ec2a9b0d4
hemanth Kumar <62005727+hemanthkb97@users.noreply.github.com>
2022-10-11 17:21:17 +0530

    removed firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -30,22 +30,3 @@ void main() async {
     ),
   );
 }
-
-class Configurations {
-  static const _apiKey = "AIzaSyACi1B8eK__w6W-8RqXREtBOgnIFfFZ9I4";
-  static const _authDomain = "collaborative-tool.firebaseapp.com";
-  static const _projectId = "collaborative-tool";
-  static const _databaseURL =
-      "https://collaborative-tool-default-rtdb.firebaseio.com";
-  static const _storageBucket = "collaborative-tool.appspot.com";
-  static const _messagingSenderId = "840479392416";
-  static const _appId = "1:840479392416:web:d736e102028adfddf13fc5";
-
-  String get apiKey => _apiKey;
-  String get appId => _appId;
-  String get authDomain => _authDomain;
-  String get databaseURL => _databaseURL;
-  String get messagingSenderId => _messagingSenderId;
-  String get projectId => _projectId;
-  String get storageBucket => _storageBucket;
-}
