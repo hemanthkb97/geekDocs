85e66e883a42985a358c7156b7f150aa6b86900d
hemanth Kumar <62005727+hemanthkb97@users.noreply.github.com>
2022-10-11 17:19:16 +0530

    removed firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -6,14 +6,15 @@ import 'package:provider/provider.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
+    // TODO: use your config here
     options: const FirebaseOptions(
-        apiKey: "AIzaSyACi1B8eK__w6W-8RqXREtBOgnIFfFZ9I4",
-        appId: "1:840479392416:web:d736e102028adfddf13fc5",
-        messagingSenderId: "840479392416",
-        projectId: "collaborative-tool",
-        authDomain: "collaborative-tool.firebaseapp.com",
-        databaseURL: "https://collaborative-tool-default-rtdb.firebaseio.com",
-        storageBucket: "collaborative-tool.appspot.com"),
+        apiKey: "",
+        appId: "",
+        messagingSenderId: "",
+        projectId: "",
+        authDomain: "",
+        databaseURL: "",
+        storageBucket: ""),
   );
   runApp(
     MultiProvider(
